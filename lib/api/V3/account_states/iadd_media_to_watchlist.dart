3cb42f15d5411cc4b6a46134e8436b4814427521
mateuszfilipek2000 <mateuszfilipek2000@gmail.com>
2022-04-30 17:23:24 +0200

    added few example tv api responses

--- lib/api/V3/account_states/iadd_media_to_watchlist.dart
+++ lib/api/V3/account_states/iadd_media_to_watchlist.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart';
 abstract class IAddMediaToWatchlist<T> {
   /// if you want to add media to watchlist, pass true as watchlist
   /// argument, if you want to delete media from watchlist, pass false
-  Future<void> markMediaAsFavorite(
+  Future<void> addMediaToWatchlist(
     T object, {
     bool watchlist,
     Future<Response> Function()? dataSource,
